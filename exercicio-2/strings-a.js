5d60d87026a38a46a6b247d001228765cd57bb77
isbllvt <isav.mendonca@gmail.com>
2025-07-17 22:36:07 -0300

    importa arquivos do exercício 2 da branch antiga

---
+++ exercicio-2/strings-a.js
@@ -0,0 +1,10 @@
+// Exercício: Manipulação de Strings
+
+const firstName = "Maria";
+const lastName = "Silva";
+const fullName = firstName + " " + lastName;
+console.log("Nome completo:", fullName);
+
+// a) Extraia o primeiro nome usando o método slice ou substring
+const parte = fullName.substring(6)
+console.log("Primeiro nome extraído:", parte)
